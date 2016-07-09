08cbe4ddf1a074aa021d154433a1080b94e3317c
wenghaoda <wenghaoda@gmail.com>
2016-07-09 14:50:19 -0700

    add config

--- config/express.js
+++ config/express.js
@@ -58,6 +58,7 @@ module.exports = function (app, passport) {
   // set views path, template engine and default layout
   app.set('views', config.root + '/app/views');
   app.set('view engine', 'jade');
+  app.engine('ejs', require('ejs').renderFile);
 
   // expose package.json to views
   app.use(function (req, res, next) {
