e011a454d10e484013be39071d7f447ade75a23d
Danhua Yang <ydh1031@gmail.com>
2016-07-10 04:04:15 -0700

    google map pole information

--- config/routes.js
+++ config/routes.js
@@ -39,6 +39,9 @@ module.exports = function (app, passport) {
     app.get('/summary', function (req, res) {
         res.render('dashboard/summary.html', temp.adminMainTemp("Dashboard", {}, {}));
     });
+    app.get('/map', function (req, res) {
+        res.render('dashboard/map.html', temp.adminMainTemp("Dashboard", {}, {}));
+    });
     // user routes
     app.get('/login', users.login);
     app.get('/signup', users.signup);
