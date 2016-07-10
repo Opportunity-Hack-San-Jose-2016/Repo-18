cf4d94c6a905391638fbf5cff285ac815414e395
MingTang <tmartin@126.com>
2016-07-09 20:45:25 -0700

    add angularjs 2

---
+++ public/js/dashboard/dashboard.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Ming on 7/9/16.
+ */
+app.controller('dashboardCtrl', ['$scope','$http',function($scope,$http) {
+    $scope.dashboardTest="<p>this is a testing msg from dashboardCtrl!</p>";
+
+}]);
